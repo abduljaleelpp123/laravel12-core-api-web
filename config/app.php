c299edc40b721452030639f6af466be961925cdd
Abdul <abduljaleel123@gmail.com>
2026-02-24 07:54:51 +0300

    test commit

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ return [
     |--------------------------------------------------------------------------
     | Application Name
     |--------------------------------------------------------------------------
-    |
+    |    
     | This value is the name of your application, which will be used when the
     | framework needs to place the application's name in a notification or
     | other UI elements where an application name needs to be displayed.
