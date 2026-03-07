b5642cd750d6fdc4a720e66c5d38e8b8700c641b
ABDUL JALEEL PP <abduljaleel123@gmail.com>
2026-03-07 17:00:47 +0300

    api route added

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    // Your protected CRUD routes will go here
+});
